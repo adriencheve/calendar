7ec219687c96c04335aa2cff7e45f3cf556ee5b5
Sandesh Damkondwar <sandeshdamkondwar@gmail.com>
2015-05-24 03:58:52 +0530

    Built filter for creating range of array

--- app/filters.js
+++ app/filters.js
@@ -13,4 +13,15 @@
             return hour + ' :00 ' + meridiem;
         };
     });
+
+    app.filter('range', function() {
+        return function(input, total) {
+            total = parseInt(total);
+            for (var i = 0; i < total; i++) {
+                input.push(i);
+            }
+
+            return input;
+        };
+    });
 })();
